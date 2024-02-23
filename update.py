1398537f278bc68939781332411d51f95efb6488
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-02-23 09:34:32 +0100

    allow script to update repo dir

--- update.py
+++ update.py
@@ -1,10 +1,13 @@
-import subprocess
+#!/bin/env python3
+
+import argparse
 import json
 import re
-from typing import Any
+import subprocess
 import urllib.request
-from pathlib import Path
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
 
 
 @dataclass
@@ -40,6 +43,17 @@ class Pkgbuild:
 
         self.path.write_text(out)
 
+        print(
+            f"Updated {pkgbuild.path} from {pkgbuild.current_version} "
+            f"to {pkgbuild.new_version}"
+        )
+
+    def update_srcinfo(self):
+        srcinfo = self.path.parent / ".SRCINFO"
+        srcinfo.write_text(srcinfo.decode("utf-8"))
+
+        print("Updated .SRCINFO")
+
     @classmethod
     def from_path(cls, path: Path):
         text = path.read_text()
@@ -66,22 +80,36 @@ class Pkgbuild:
 
         return Pkgbuild(path, text, data, current_version, new_version)
 
+def main(path, check):
+    pkgbuild = Pkgbuild.from_path(path)
 
-if __name__ == "__main__":
-    pkgbuild = Pkgbuild.from_path(Path("PKGBUILD"))
     if not pkgbuild.check_update():
-        print(f"{pkgbuild.path.absolute()} is already up to date")
+        print(f"{pkgbuild.path} is already up to date")
         exit(0)
+
+    print(
+        f"{pkgbuild.path} is out of date. "
+        f"Current version: {pkgbuild.current_version}, "
+        f"New version: {pkgbuild.new_version}"
+    )
+
+    if check:
+        return 0
 
     pkgbuild.update_pkgbuild()
 
-    print(
-        f"Updated {pkgbuild.path.absolute()} from {pkgbuild.current_version} "
-        f"to {pkgbuild.new_version}"
-    )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Update PKGBUILD")
+    parser.add_argument("path", type=Path, help="Path to package directory")
+    parser.add_argument("--check", action="store_true", help="Check for updates")
+    parser.add_argument("--repo", action="store_true", help="Directory is repository of packages, not a single package")
+    parser.add_argument("--skip", nargs="+", help="Names of packages to skip", default=["dura-git"])
 
-    srcinfo = subprocess.check_output(["makepkg", "--printsrcinfo"])
+    args = parser.parse_args()
 
-    Path(".SRCINFO").write_text(srcinfo.decode("utf-8"))
+    paths = Path(args.path).glob("*/PKGBUILD") if args.repo else[Path(args.path)]
 
-    print("Updated .SRCINFO")
+    for path in paths:
+        if path.parent.name in args.skip:
+            continue
+        main(path, check=args.check)
